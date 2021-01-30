4492447352fe6f24b553a5e164d247ab8a8c9e2d
rxdanny <44015667+RelaxDanny@users.noreply.github.com>
2021-01-31 02:43:53 +0900

    Clone Coding Set up

---
+++ App.js
@@ -0,0 +1,27 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow strict-local
+ */
+
+import React from 'react';
+import {
+  SafeAreaView,
+  StatusBar,
+} from 'react-native';
+
+import Entype from 'react-native-vector-icons/Entypo'
+const App: () => React$Node = () => {
+  return (
+    <>
+      <StatusBar barStyle="dark-content" />
+      <SafeAreaView>
+        
+      </SafeAreaView>
+    </>
+  );
+};
+
+export default App;
---
+++ ios/Pods/Target Support Files/Pods-clone-cloneTests/Pods-clone-cloneTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_clone_cloneTests : NSObject
+@end
+@implementation PodsDummy_Pods_clone_cloneTests
+@end
